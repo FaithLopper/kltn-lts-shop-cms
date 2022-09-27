0c687ba895bd14ff8de24d2654c1ad74a72756cd
Lô Minh Phát <lmpitmail0701@gmail.com>
2022-09-27 15:16:56 +0700

    fix index

--- source/src/routes/index.js
+++ source/src/routes/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Switch, BrowserRouter, Redirect } from 'react-router-dom';
+
 import { sitePathConfig } from '../constants/sitePathConfig';
+
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+
+import LoginPage from '../containers/account/LoginPage';
+import ProfilePage from '../containers/account/ProfilePage';
+
+// import DashBoard from '../containers/Dashboard';
+import UserAdminListPage from '../containers/users/UserAdminListPage';
+
 import NotFound from '../compoments/common/NotFound';
 import Forbidden from '../containers/Forbidden';
-
+import GroupPermissionListPage from '../containers/groupPermission/GroupPermissionListPage';
+import CategoryListPage from '../containers/category/CategoryListPage';
+import CategoryListPageChild from '../containers/category/CategoryListPageChild';
+import NewsListPage from '../containers/adminNews/NewsListPage';
+import UserAminUpdate from '../containers/users/UserAminUpdate';
+import ProvinceListPage from '../containers/province/ProvinceListPage';
+import DistrictListPage from '../containers/province/DistrictListPage';
+import CommuneListPage from '../containers/province/CommuneListPage';
 const RootRoute = () => {
     const {
         admin,
+        login,
+        profile,
         forbidden,
+        groupPermission,
+        category,
+        adminNews,
+        adminUpdate,
+        province,
+        provinceUpdate
     } = sitePathConfig;
 
     return (
